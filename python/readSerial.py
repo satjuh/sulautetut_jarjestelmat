14c88223e68549afbf80f5b63509a4c1d18222fb
Juhani Säteri <juhani.sateri@tuni.fi>
2020-08-25 00:15:08 +0300

    Simple python script to read serial

--- python/readSerial.py
+++ python/readSerial.py
@@ -8,6 +8,7 @@ import sys
 def signal_handler(sig, frame):
     print("ctr+c quitting...")
     sys.exit(0)
+
 # read serial input every second and print output
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal_handler)
